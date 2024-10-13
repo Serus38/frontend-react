1312db7bff7be6cf5c32bdd4c9e8e1e4a0cb18a1
Sergio Rivera <95110101+Serus38@users.noreply.github.com>
2024-10-12 22:46:44 -0500

    render

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/'
+    // baseURL: 'http://localhost:3001/'
+    baseURL: 'https://api-rest-2yv0.onrender.com'
 });
 
 export {
